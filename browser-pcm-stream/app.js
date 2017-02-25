ca205993ee4bc3318135221f32f22b653ff58fdc
bishalgautam <bishal.htd04@gmail.com>
2017-02-25 17:12:21 -0600

    commit the JavaScript

---
+++ browser-pcm-stream/app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var BinaryServer = require('binaryjs').BinaryServer;
+var fs = require('fs');
+var wav = require('wav');
+
+var port = 3700;
+var outFile = 'demo.wav';
+var app = express();
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/api', function(req,res){
+  res.render('page');
+});
+
+app.listen(port);
+
+console.log('server open on port ' + port);
+
+binaryServer = BinaryServer({port: 9001});
+
+binaryServer.on('connection', function(client) {
+  console.log('new connection');
+
+  var fileWriter = new wav.FileWriter(outFile, {
+    channels: 1,
+    sampleRate: 48000,
+    bitDepth: 16
+  });
+
+  client.on('stream', function(stream, meta) {
+    console.log('new stream');
+    stream.pipe(fileWriter);
+
+    stream.on('end', function() {
+      fileWriter.end();
+      console.log('wrote to file ' + outFile);
+    });
+  });
+});
---
+++ browser-pcm-stream/public/voice.js
@@ -0,0 +1,84 @@
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+
+// Add a single command
+var commandHello = {
+    indexes:["hello","lets see what u got","hey", "i am home"], // These spoken words will trigger the execution of the command
+    action:function(){ // Action to be executed when a index match with spoken word
+        //artyom.say("Hey buddy ! How are you today?");
+     while(true){
+        startRecognition();
+        wait(20000); //waits for 20 seconds.
+        stopRecognition();
+     }  
+        
+    }
+};
+
+artyom.addCommands(commandHello); // Add the command with addCommands method. Now
+
+// print the text that is recognized 
+
+artyom.redirectRecognizedTextOutput(function(recognized,isFinal){
+    var span = document.getElementById('output');
+
+    if(isFinal){
+        span.innerHTML = '';
+        console.log("Final recognized text: " + recognized);
+    }else{
+        span.innerHTML = recognized;
+        console.log(recognized);
+    }
+});
+
+function simulate(){
+  artyom.simulateInstruction("home");
+}
+// function that initializes  and starts listening 
+function startOneCommandArtyom(){
+
+    artyom.fatality();// use this to stop any of
+
+     setTimeout(function(){// if you use artyom.fatality , wait 250 ms to initialize again.
+         artyom.initialize({
+            lang:"en-US",// A lot of languages are supported. Read the docs !
+            continuous:false,// recognize 1 command and stop listening !
+            listen:true, // Start recognizing
+            debug:true, // Show everything in the console
+            speed:1 // talk normally
+        });
+    },250);
+}
+
+// speech to text 
+
+var settings = {
+    continuous:false, // Don't stop never because i have https connection
+    onResult:function(text){
+        // text = the recognized text
+        console.log(text);
+    },
+    onStart:function(){
+        console.log("Dictation started by the user");
+    },
+    onEnd:function(){
+        console.log("Dictation stopped after 10 sec");
+    }
+};
+
+var UserDictation = artyom.newDictation(settings);
+
+function startRecognition(){
+  UserDictation.start();
+}
+
+function stopRecognition(){
+  UserDictation.stop();
+
+}
