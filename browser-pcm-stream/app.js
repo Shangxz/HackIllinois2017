5b04ce574a5d66feae3af9f3be7ed8a76d71c362
bishalgautam <bishal.htd04@gmail.com>
2017-02-26 03:29:16 -0600

    ssl layer

--- browser-pcm-stream/app.js
+++ browser-pcm-stream/app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var BinaryServer = require('binaryjs').BinaryServer;
-var fs = require('fs');
+//var fs = require('fs');
 var wav = require('wav');
 
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('/etc/ssl/private/server.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/server.crt')
+};
+
 var port = 3700;
+var porthttps = 8443;
 var outFile = 'demo.wav';
 var app = express();
 
@@ -22,7 +31,13 @@ app.get('/api', function(req,res){
 
 app.listen(port);
 
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("Welcome to Node.js HTTPS Servern");
+}).listen(porthttps);
+
 console.log('server open on port ' + port);
+onsole.log('https server open on port '+ porthtpps);
 
 binaryServer = BinaryServer({port: 9001});
 
